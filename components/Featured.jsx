0c26fc127b5f6f10ed5d3da67e6e3b91cc4486ae
cjunior3001 <jrpractice@hotmail.com>
2022-01-15 18:52:30 -0300

    first commit and menu

---
+++ components/Featured.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Featured = () => {
+  return (
+    <div>
+      f
+    </div>
+  )
+}
+
+e
+import Image from 'next/image'xport default Featured
+f
